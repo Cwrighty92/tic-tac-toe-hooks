eda939bdbacd6f6ce22b3fdb9c80f526994dab0b
Chris Wright <chriswrighty1192@gmail.com>
2020-01-25 14:14:21 +0000

    remove game info for now

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -4,10 +4,6 @@ import { Board } from "./board";
 export const Game = () => (
   <div className="game">
     <div className="game-board">{<Board />}</div>
-    <div className="game-info">
-      <div>{/* status */}</div>
-      <ol>{/* TODO */}</ol>
-    </div>
   </div>
 );
 
