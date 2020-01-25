7d5abd86a594cf5049c2a2e6b4af9a37b9127def
Chris Wright <chriswrighty1192@gmail.com>
2020-01-25 14:16:59 +0000

    rename lines to winning lines

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -67,7 +67,7 @@ export const Board = () => {
 };
 
 function calculateWinner(squares: string[]) {
-  const lines = [
+  const winningLines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -77,8 +77,8 @@ function calculateWinner(squares: string[]) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
